714382e2db4372dc735e87d2f49e2ab09eb1a0d7
efree98 <elizabeth_free@outlook.com>
2020-05-04 12:03:44 -0500

    Current Update: 12:02  P.M. 5/4/2020, Reran Migrations, The darn thing is actually running slightly, for at least a few seconds

---
+++ Migrations/20200504164330_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Final_Project_Alpha.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GameTitle = table.Column<string>(nullable: true),
+                    Rating = table.Column<string>(nullable: true),
+                    Price = table.Column<string>(nullable: true),
+                    GameConsole = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.GameId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Publisher",
+                columns: table => new
+                {
+                    PublisherId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Publishing_Company = table.Column<string>(nullable: true),
+                    GameId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publisher", x => x.PublisherId);
+                    table.ForeignKey(
+                        name: "FK_Publisher_Game_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Game",
+                        principalColumn: "GameId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publisher_GameId",
+                table: "Publisher",
+                column: "GameId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Publisher");
+
+            migrationBuilder.DropTable(
+                name: "Game");
+        }
+    }
+}
