02dc93c8be4e41f195d4e2932963d4e5cc6c13e5
efree98 <elizabeth_free@outlook.com>
2020-05-03 22:15:44 -0500

    Current Update: 10:15  P.M. 5/3/2020,Finally no problems, can now continue as planned

---
+++ Pages/Games/AddPublisher.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using FinalProject.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Final_Project_Alpha.Pages.Games
+{
+    public class AddGameModel : PageModel
+    {
+        private readonly ILogger<AddGameModel> _logger;
+        private readonly GameDbContext _context; // Game Database context
+        [BindProperty]
+        public Publisher Publisher {get; set;}
+        public SelectList GameDropDown {get; set;}
+
+        public AddGameModel(GameDbContext context, ILogger<AddGameModel> logger)
+        {
+            // Bring in Database context and logger using dependency injection
+            _context = context;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            GameDropDown = new SelectList(_context.Game.ToList(), "GameId", "GameTitle");
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            
+            _context.Add(Publisher);
+            _context.SaveChanges();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
