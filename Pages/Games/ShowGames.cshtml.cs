18c5c149e1cd9888294278f36a624b393ec181cb
efree98 <elizabeth_free@outlook.com>
2020-05-05 01:49:02 -0500

    Current Update: 1:48 A.M., Making good progress, having issues with displaying data

---
+++ Pages/Games/ShowGames.cshtml.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FinalProject.Models;
+
+namespace Final_Project_Alpha.Pages.Games
+{
+    public class ShowGameModel : PageModel
+    {
+        private readonly FinalProject.Models.GameDbContext _context;
+
+        public ShowGameModel(FinalProject.Models.GameDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Game Game { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Game = await _context.Game.FirstOrDefaultAsync(m => m.GameId == id);
+
+            if (Game == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        } 
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Game = await _context.Game.FindAsync(id);
+
+            if (Game != null)
+            {
+                _context.Game.Remove(Game);
+                await _context.SaveChangesAsync();
+            }  
+
+            
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
